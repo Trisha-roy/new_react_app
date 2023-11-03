fd42cb735c3bbf36f90dc821a8196abf6edec92c
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-11-02 23:45:12 -0300

    Added conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './todo'
 
 function App() {
 
@@ -11,7 +12,9 @@ function App() {
       
       <h1>Vite + React</h1>
       <Person></Person>
+      <Todo task="Learn React"></Todo>
      
+      <Todo task="Learn React"></Todo>
       
     </>
   )
---
+++ src/todo.jsx
@@ -0,0 +1,5 @@
+export default function Todo({task}){
+    return(
+        <li>Task 1 is: {task}</li>
+    )
+}
