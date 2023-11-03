ff4bbf6adf276f27224f243afcb656f0e66e0b35
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-11-03 13:34:35 -0300

    Added Singer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Todo from './todo'
+import Actor from './Actor'
+import Singer from './Singer'
 
 function App() {
-
+  const actors=['jasim','raj','sakib','jeet','sharukh','hrittik']
+  const singers=[
+    {name:'Pritom Hasan',age:28},
+    {name:'Trisha Roy',age:24}
+  ]
 
   return (
     <>
       
       <h1>Vite + React</h1>
       <Person></Person>
+      {
+        singers.map(singer=><Singer singer={singer}></Singer>)
+      }
+      <Actor name={"Dev Adhikari"}></Actor>
+      {
+        actors.map(actor=><Actor name={actor}></Actor>)
+      }
       <Todo task="Learn React"></Todo>
      
       <Todo task="Learn React"></Todo>
